0a4b1e0786153ea45bfc63f3f5f47d80c81f77bb
mustafa-farag <mustafafarag978@gmail.com>
2020-06-03 02:19:12 -0700

    show appointment statistics & edit appointment for admin

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using DoctorPatient.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Infrastructure;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private HospitalDbContext db = new HospitalDbContext();
         public ActionResult Index()
         {
             return View();
@@ -15,9 +18,14 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            IQueryable<AppointmentDateGroupViewModel> data = from appointment in db.Appointments
+                                                             group appointment by appointment.Date into dateGroup
+                                                             select new AppointmentDateGroupViewModel()
+                                                             {
+                                                                 AppointmentDate = dateGroup.Key,
+                                                                 PatientCount = dateGroup.Count()
+                                                             };
+            return View(data.ToList());
         }
 
         public ActionResult Contact()
